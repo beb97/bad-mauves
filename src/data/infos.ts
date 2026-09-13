fc5a25ffc6a3695e6d2b1cba0204217f5cb67591
beb97 <beb97@hotmail.com>
2026-09-13 15:28:46 +0200

    Updated infos headers

---
+++ src/data/infos.ts
@@ -0,0 +1,33 @@
+import type {InfoLink} from "@components/info/InfoCard.astro";
+
+export const infos : InfoLink[] = [
+    {
+        logo: 'badminton',
+        nom: "inscription",
+        lien: '/infos/acces',
+    },	{
+        logo: 'schedule',
+        nom: "horaires",
+        lien: '/infos/horaires',
+    },
+    {
+        logo: 'stadium️',
+        nom: "salle",
+        lien: '/infos/salle',
+    },
+    {
+        logo: 'history',
+        nom: "histoire",
+        lien: '/infos/histoire',
+    },
+    {
+        logo: 'diversity_4',
+        nom: "bureau",
+        lien: '/infos/membres',
+    },
+    {
+        logo: 'gavel',
+        nom: "règles",
+        lien: '/infos/rules',
+    },
+];
