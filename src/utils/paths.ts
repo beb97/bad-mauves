1719ada0b886cacc69bed03af968b5d9eee194c0
beb97 <beb97@hotmail.com>
2026-09-13 13:28:16 +0200

    Fixed back buton navigation

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,3 +1,10 @@
 export const withBase = (path: string) => {
     return `${import.meta.env.BASE_URL}${path.replace(/^\//, "")}`;
 };
+
+export const goBack = (event: Event) => {
+    if (window.history.length > 1) {
+        event.preventDefault();
+        window.history.back();
+    }
+};
